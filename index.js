3f202f862c71569941430105514a8212d17a385a
nzi19001@byui.edu <nzi19001@byui.edu>
2023-12-19 17:43:01 +0300

    completed asignment

---
+++ index.js
@@ -0,0 +1,33 @@
+// Arrow Function Shortcuts
+const playMusic = function (music) {
+    return 'playing' + music;
+
+}
+playMusic('jazz');
+
+const playMusics = (music) => {
+    return `i love playing ${music}`
+}
+playMusics('drums')
+
+let myfunction = (a, b) => a * b;
+myfunction(1, 2)
+
+
+hello = () => {
+    return "Hello World";
+
+
+}
+
+hello = () => "Hello World"
+console.log('hello world')
+
+function greetings(name) {
+    return `Hello am ${name}`
+}
+const greetings = (name) => {
+    return `Hello am ${name}`
+}
+greetings = (name) => `Hello ${name}`
+console.log(greetings('Hello am Josephine'))
